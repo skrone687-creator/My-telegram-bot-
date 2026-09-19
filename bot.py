c1de5d2ecb0ee79700d5722fe38a1c2646fcec4e
skrone687-creator <skrone687@gmail.com>
2026-09-19 23:42:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,10 +109,8 @@ async def send_welcome(message: types.Message):
         parse_mode="HTML",
         reply_markup=main_menu_kb(),
     )
-
-
-       await callback_query.answer()
-       await callback_query.message.edit_text(
+        await callback_query.answer()
+        await callback_query.message.edit_text(
         "यहाँ हमारे प्रोडक्ट्स की लिस्ट है:",
         reply_markup=your_products_kb()
     )
