258b4a3f6e3702d82f4101b8c92c6587367ac65b
skrone687-creator <skrone687@gmail.com>
2026-09-19 20:13:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -233,7 +233,7 @@ async def process_add_amount(callback: types.CallbackQuery):
  amount = callback.data.split("_")[1]
  await callback.message.edit_text(
  f"You selected {amount}. Proceeding to payment..."
-
+ )
 if __name__ == '__main__':
     dp.include_router(router)
     asyncio.run(dp.start_polling(bot))
