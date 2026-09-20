44e6e9844bd9dfd78359bf49d5dc082397fd7bb4
skrone687-creator <skrone687@gmail.com>
2026-09-20 14:55:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -298,13 +298,14 @@ async def menu_add_balance(call: types.CallbackQuery):
 
 @router.callback_query(F.data == "how_to_bot")
 async def process_how_to_use(call: types.CallbackQuery):
-    text = (
-        "<blockquote>❗ How to use this bot:</blockquote>\n\n"
+        text = (
+        "<blockquote>❗️ How to use this bot:</blockquote>\n\n"
         "• Add balance via Sahil bhai Secure QR System or Binance Pay\n"
         "• Tap Buy Now and pick your desired product\n"
         "• Key is delivered instantly to this chat screen\n"
         "• Browse plans and checkout seamlessly"
     )
+
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         InlineKeyboardButton(text="🎬 VIEW TUTORIAL VIDEO ", url="https://t.me/sahil_bhai_69/6", style="success")
         [InlineKeyboardButton(text="Back", callback_data="menu_back", style="danger")]
