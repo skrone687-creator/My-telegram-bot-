eabe4e308b02541330873066a73b1297bb985055
skrone687-creator <skrone687@gmail.com>
2026-09-20 12:59:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -279,10 +279,12 @@ async def process_daily_gift(callback_query: types.CallbackQuery):
 @router.callback_query(F.data == "menu_add_balance")
 async def menu_add_balance(call: types.CallbackQuery):
     text = (
-        "<blockquote>\n"
-        "💰 <b>ADD FUNDS TO WALLET</b> \n\n"
+        "<blockquote>"
+        "💰 <b>ADD FUNDS TO WALLET</b> 💰"
+        "</blockquote>\n\n"
         "Choose a quick amount to add or type/use a custom one below.\n\n"
-        "🚀 <i>Predefined amounts are faster to process!</i>\n"
+        "<blockquote>"
+        "🚀 <i>Predefined amounts are faster to process!</i>"
         "</blockquote>"
     )
     await call.message.edit_text(
