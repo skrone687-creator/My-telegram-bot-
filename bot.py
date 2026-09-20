694ceefdd1cb448b3b3951a41a95d277748cb03b
skrone687-creator <skrone687@gmail.com>
2026-09-20 11:26:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,16 +131,9 @@ def update_kb():
     kb.inline_keyboard[0][0].style = "danger"
     return kb
 
-@router.callback_query(F.data == "menu_check_update")
+@dp.callback_query(F.data == "menu_check_update")
 async def process_check_update(call: types.CallbackQuery):
-    await call.message.edit_message_media(
-    media=InputMediaPhoto(
-        media="AgACAgUAAxkBAA—u621xOxS3DNMRK6j0TRvuqx1j8XAACSrJrGWABUnhVfq",
-        caption="<blockquote>Follow our updates channel:</blockquote>\n\n🔗 <a href='https://t.me/sahilbhaiallupdate'>Click Here For Setup & Updates</a>",
-        parse_mode="HTML"
-    ),
-    reply_markup=update_kb()
-)
+    
     await call.answer()
 
 
