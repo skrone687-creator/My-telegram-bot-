e2a6a37c7a0f3e7bbf7ab1e7413832bba557d4a8
skrone687-creator <skrone687@gmail.com>
2026-09-20 18:58:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ products_db = {
 }
 
 # बोट सेटअप
-API_TOKEN = '8955117111:AAGGqUdqe4AtpkAXlzfNQXb6UfC264EDT5g'
+API_TOKEN = '8616026448:AAHljhqijm3giJAXbVubYgk1SZNo4gYEKGk'
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
