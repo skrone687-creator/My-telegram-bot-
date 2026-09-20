7efc85133258ccdb59c302a52987c4a11015ebd4
skrone687-creator <skrone687@gmail.com>
2026-09-20 15:18:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -306,7 +306,7 @@ async def process_how_to_use(call: types.CallbackQuery):
         "• Browse plans and checkout seamlessly"
     )
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
- [InlineKeyboardButton(text="» VIEW TUTORIAL VIDEO «", url="https://t.me/sahil_bhai_69/6", style="success")],
+ [InlineKeyboardButton(text="🎬 VIEW TUTORIAL VIDEO ", url="https://t.me/sahil_bhai_69/6", style="success")],
  [InlineKeyboardButton(text="« Back", callback_data="menu_back", style="danger")]
 ])
     await call.message.edit_text(text=text, parse_mode="HTML", reply_markup=keyboard)
