df77869467208fb21218152be0b52a18bff85ae3
skrone687-creator <skrone687@gmail.com>
2026-09-21 21:06:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -145,7 +145,7 @@ async def send_welcome(message: types.Message):
     await message.answer(
         "<blockquote>🏪 SAHIL BHAI STORE 🔓</blockquote>\n"
         
-        "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n\n"
+        "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n\n\n\n"
         "┝  🛍️ Buy Now : All Key Purchase & Instant Delivery\n"
         "┝  📢 Check Update : Check Setup Video And Update Apk\n"
         "┝  🪙 Add Balance : Deposit Balance & Secure Auto-Add Payment System\n"
