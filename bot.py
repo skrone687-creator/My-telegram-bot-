7f2ce053884492ef05c39b67dd60d85e780d567f
skrone687-creator <skrone687@gmail.com>
2026-09-21 22:27:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -302,7 +302,7 @@ async def menu_support(call: types.CallbackQuery):
 async def process_menu_back(call: types.CallbackQuery):
     await call.message.edit_text(
     "<blockquote><b>🏪 SAHIL BHAI STORE 🔓</b></blockquote>"
-    "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n\n"
+    "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n"
     "┝🛍️ Buy Now : All Key Purchase & Instant Delivery"
     "┝📢 Check Update : Check Setup Video And Update Apk"
     "┝🪙 Add Balance : Deposit Balance & Secure Auto-Add Payment System"
@@ -312,7 +312,8 @@ async def process_menu_back(call: types.CallbackQuery):
     "┝📨 Support : Bot Problem Fixed For Support Admin\n"
     "┝🎁 Daily Gift : Free Spin and win random balance daily, Only one spin every 24 hours."
     "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️"
-    "<blockquote>💰 Your 🪙 Balance: ₹0.00</blockquote>"
+    "<blockquote>💰 Your 🪙 Balance: ₹0.00</blockquote>\n"
+    "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n"   
     "<i>Select an option from the menu below:</i>",
     parse_mode="HTML",
     reply_markup=main_menu_kb(),
