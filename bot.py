552412490bb6288961f3d7c284085bdd0b1647f6
skrone687-creator <skrone687@gmail.com>
2026-09-22 00:28:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -184,7 +184,7 @@ async def process_check_update(call: types.CallbackQuery):
 @router.callback_query(F.data.startswith("amount_"))
 async def process_amount(callback_query: types.CallbackQuery):
     amount = callback_query.data.split("_")[1]
-    print(callback_query.data)
+    print(call.data)
     text = (
         f"<blockquote><b>SELECT GATEWAY MODE</b></blockquote>\n\n"
         f"💰 Deposit Amount: 🪙 {amount}.00"
