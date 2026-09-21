d68371f7764225d390813ebf99962d4694c3498e
skrone687-creator <skrone687@gmail.com>
2026-09-21 19:50:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -247,7 +247,7 @@ async def refer_and_earn(call: types.CallbackQuery):
     )
 
     keyboard = InlineKeyboardMarkup(row_width=1)
-    keyboard.add(
+    keyboard.add()
         InlineKeyboardButton(" Back", callback_data="menu_back")
 from aiogram import 
 from aiogram import F, Router, types
