61739221344e062c9f8ada88cca0e7d453bd3793
skrone687-creator <skrone687@gmail.com>
2026-09-21 17:48:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ def main_menu_kb() -> InlineKeyboardMarkup:
     return kb
 
 # यहाँ आप अपने कमान्ड हैंडर्स जोड़ सकते हैं
-@dp.message(Command("start"))
+@router.message(Command("start"))
 async def send_welcome(message: types.Message):
     await message.answer(
         "<blockquote>🏪 SAHIL BHAI STORE 🔓</blockquote>\n"
