c5b7e303467b1aa0fad43a635cb4ae626e144bad
skrone687-creator <skrone687@gmail.com>
2026-09-22 14:23:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -600,7 +600,7 @@ async def process_main_menu(call: types.CallbackQuery):
 @router.callback_query(F.data == "menu_shop")
 async def process_menu_shop(call: types.CallbackQuery):
     keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
-                keyboard_buttons = []
+    keyboard_buttons = []
 
     for id, name in products_db.items():
         keyboard_buttons.append(
