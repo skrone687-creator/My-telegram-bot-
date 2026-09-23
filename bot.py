0534d82fa33201b6cd9ddffdcaccf99c85d0c7d7
skrone687-creator <skrone687@gmail.com>
2026-09-24 00:44:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -269,10 +269,10 @@ async def process_profile(callback_query: types.CallbackQuery):
     active_referrals = "0"
     
     profile_text = (
-    "<blockquote>👤 YOUR PROFILE DASHBOARD 👤</blockquote>\n\n"
+    "<blockquote>👑 YOUR PROFILE DASHBOARD </blockquote>\n\n"
     f"<blockquote>🆔 Account ID: <code>{account_id}</code></blockquote>\n"
-    "<blockquote>💎 Account Tier: USER</blockquote>\n"
-    f"<blockquote>💰 Current Funds: {current_funds}</blockquote>\n"
+    "<blockquote>✨ Account Tier: USER</blockquote>\n"
+    f"<blockquote>🪙 Current Funds: {current_funds}</blockquote>\n"
     f"<blockquote>👥 Active Referrals: {active_referrals} users</blockquote>\n\n"
     "Select history filter panel below to view records."
 )
