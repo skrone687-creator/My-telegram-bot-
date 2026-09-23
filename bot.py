4a998f590f5ac01474f34164cc1f7b1bccca8815
skrone687-creator <skrone687@gmail.com>
2026-09-23 20:19:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -548,7 +548,6 @@ async def process_pay_upi(callback_query: types.CallbackQuery, state: FSMContext
 
 @router.callback_query(F.data == "cancel_order")
 async def cancel_order(call: types.CallbackQuery):
-    await call.message.delete()
     await menu_add_balance(call)
 
 
