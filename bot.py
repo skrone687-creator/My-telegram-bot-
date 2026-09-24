f681473282365f0fd6ecefe9ee6210aed4ca76c3
skrone687-creator <skrone687@gmail.com>
2026-09-24 19:15:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -211,7 +211,7 @@ async def send_welcome(message: types.Message):
      "┝📨 Support : Bot Problem Fixed For Support Admin\n"
      "┝🎁 Daily Gift : Free Spin and win random balance daily, Only one spin every 24 hours.\n\n"
      "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n\n"
-     "<blockquote>💰 Your Balance:🪙₹{current_balance}</blockquote>\n\n"  
+     f"<blockquote>💰 Your Balance:🪙₹{current_balance}</blockquote>\n\n"  
      "<i>👇 Select an option from the menu below:</i>",
      parse_mode="HTML",
      reply_markup=main_menu_kb(),
@@ -383,7 +383,7 @@ async def process_menu_back(call: types.CallbackQuery):
     "┝📨 Support : Bot Problem Fixed For Support Admin\n"
     "┝🎁 Daily Gift : Free Spin and win random balance daily, Only one spin every 24 hours.\n\n"
     "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️\n\n"
-    "<blockquote>💰 Your  Balance:🪙₹{current_balance}</blockquote>\n\n"  
+    f"<blockquote>💰 Your  Balance:🪙₹{current_balance}</blockquote>\n\n"  
     "<i>👇 Select an option from the menu below:</i>",
     parse_mode="HTML",
     reply_markup=main_menu_kb(),
