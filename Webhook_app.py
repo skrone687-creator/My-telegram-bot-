c4ccd57be47dcd8bdfdbd8fbe5225b3fe39727fd
skrone687-creator <skrone687@gmail.com>
2026-09-26 21:02:17 +0530

    Update Webhook_app.py

--- Webhook_app.py
+++ Webhook_app.py
@@ -1,9 +1,1 @@
-from fastapi import FastAPI, Request
 
-app = FastAPI()
-
-@app.post("/webhook")
-async def receive_webhook(request: Request):
-    data = await request.json()
-    print(data)
-    return {"status": "success"}
